9704dd2381261c5f5944ace8df026353d484c17e
Ben Silveston <bsilveston37@gmail.com>
2021-01-20 17:22:04 +0000

    Updated the app.js file.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,15 @@
-const username = document.getElementById("usrname").value;
-const firstName = document.getElementById("fname").value;
-const lastName = document.getElementById("lname").value;
-const email = document.getElementById("email").value;
-const password = document.getElementById("psw").value;
+const username = document.getElementById("username");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("psw");
 
+const inputFields = document.getElementsByTagName("input");
+const inputFieldIndicators = document.querySelectorAll(".fa-times");
 const form = document.getElementById("form");
-const errorSection = document.getElementsByClassName("error-section");
+const formSubmissionSection = document.getElementById(
+	"form-submission-section"
+);
 const resetButton = document.getElementById("reset-button");
 const submitButton = document.getElementById("submit");
 
@@ -15,36 +19,106 @@ const emailRegex = new RegExp(
 );
 const passwordRegex = new RegExp(/^[a-z0-9_-]{6,18}$/);
 
-const validateForm = () => {
-	const inputFields = document.querySelectorAll("input");
-	const inputFieldIndicators = document.getElementsByClassName(
-		"fas fa-times"
-	);
-
-	for (let i = 0; i < inputFields.length; i++) {
-		if (
-			inputFields[i].value.trim() !== "" &&
-			(inputFields[i].match(nameRegex) ||
-				inputFields[i].match(emailRegex) ||
-				inputFields[i].match(passwordRegex))
-		) {
-			console.log("Fields have been filled successfully.");
-		}
+username.addEventListener("keyup", () => {
+	const checked = inputFieldIndicators[0];
+	if (
+		username.value.trim() !== "" &&
+		username.value.trim().match(nameRegex)
+	) {
+		checked.className = "fas fa-check";
+	} else {
+		checked.className = "fas fa-times";
 	}
-	return true;
-};
+});
+
+password.addEventListener("keyup", () => {
+	const checked = inputFieldIndicators[1];
+	if (
+		password.value.trim() !== "" &&
+		password.value.trim().match(passwordRegex)
+	) {
+		checked.className = "fas fa-check";
+	} else {
+		checked.className = "fas fa-times";
+	}
+});
+
+if (email) {
+	email.addEventListener("keyup", () => {
+		const checked = inputFieldIndicators[2];
+		if (email.value.trim() !== "" && email.value.trim().match(emailRegex)) {
+			checked.className = "fas fa-check";
+		} else {
+			checked.className = "fas fa-times";
+		}
+	});
+}
+
+if (firstName) {
+	firstName.addEventListener("keyup", () => {
+		const checked = inputFieldIndicators[3];
+		if (
+			firstName.value.trim() !== "" &&
+			firstName.value.trim().match(nameRegex)
+		) {
+			checked.className = "fas fa-check";
+		} else {
+			checked.className = "fas fa-times";
+		}
+	});
+}
+
+if (lastName) {
+	lastName.addEventListener("keyup", () => {
+		const checked = inputFieldIndicators[4];
+		if (
+			lastName.value.trim() !== "" &&
+			lastName.value.trim().match(nameRegex)
+		) {
+			checked.className = "fas fa-check";
+		} else {
+			checked.className = "fas fa-times";
+		}
+	});
+}
 
 const resetForm = (e) => {
 	e.preventDefault();
-	form.reset();
-};
-
-const submitFormData = () => {
-	if (validateForm) {
+	for (inputField of inputFields) {
+		if (inputField.value.trim() !== "") {
+			inputField.value = "";
+		}
+	}
+	for (inputFieldIndicator of inputFieldIndicators) {
+		if (inputFieldIndicator.className === "fas fa-check") {
+			inputFieldIndicator.className = "fas fa-times";
+		}
 	}
 };
 
-// validateForm();
+const submitForm = (e) => {
+	e.preventDefault();
+	for (inputField of inputFields) {
+		if (
+			username.value.trim() !== "" &&
+			password.value.trim() !== "" &&
+			email.value.trim() !== ""
+		) {
+			if (
+				inputField.value.trim() !== "" &&
+				(inputField.value.trim().match(nameRegex) ||
+					inputField.value.trim().match(emailRegex) ||
+					inputField.value.trim().match(passwordRegex))
+			) {
+				formSubmissionSection.className = "submitted";
+				formSubmissionSection.innerHTML = `<p>Form has been submitted successfully.</p>`;
+			}
+		} else {
+			formSubmissionSection.className = "not-submitted";
+			formSubmissionSection.innerHTML = `<p>Cannot submit form. Please try again.</p>`;
+		}
+	}
+};
 
 resetButton.addEventListener("click", resetForm);
-submitButton.addEventListener("click", submitFormData);
+submitButton.addEventListener("click", submitForm);
